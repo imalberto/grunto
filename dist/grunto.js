b13aca7de7e4cc86085772b251bac7f484161f47
imalberto <imalberto@gmail.com>
2014-03-01 18:34:03 -0800

    Initial import

---
+++ dist/grunto.js
@@ -0,0 +1,12 @@
+/*! grunto - v0.1.0 - 2013-03-14
+* https://github.com/imalberto/grunto
+* Copyright (c) 2013 alberto chan; Licensed MIT */
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
